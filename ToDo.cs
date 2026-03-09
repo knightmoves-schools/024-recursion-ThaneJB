7f76ad3d63d45c66bfcd3e50237734c042fc5849
Thane <thabay0606@students.maquoketaschools.org>
2026-03-09 14:24:32 -0500

    I think? Maybe?

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,14 @@
 public class ToDo
 {
     // Add your code here
+    public string[] MarkAsDone(string[] todos){
+        todos[i] = "done - " + todos[i]
+        i++;
+
+        if(index < todos.Length){
+            return MarkAsDone(todos);
+        } else {
+            return todos;
+        }
+    }
 }
