d5c7211e2877030d925a47563736d45d90dc16ff
Thane <thabay0606@students.maquoketaschools.org>
2026-03-09 14:26:07 -0500

    Forgot semicolon on line 6.

--- ToDo.cs
+++ ToDo.cs
@@ -3,7 +3,7 @@ public class ToDo
 {
     // Add your code here
     public string[] MarkAsDone(string[] todos){
-        todos[i] = "done - " + todos[i]
+        todos[i] = "done - " + todos[i];
         i++;
 
         if(index < todos.Length){
